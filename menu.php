ca4b5aaa3cf9265e651a2be1a3092131d49cb157
Kobury01 <awkwardseals@gmail.com>
2024-08-30 13:34:56 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -5,7 +5,7 @@
 <p>
  <a href="index.php">Strona główna</a>
  <a href="myProgress.php">Mój postęp</a>
- <a href="myReports.php">Moje zgłoszenia</a>
+ <a href="admin.php">Panel administratora</a>
  Witaj <?= $_SESSION["login"] ?>!
  <a href="logout.php">Wyloguj</a>
 </p>
